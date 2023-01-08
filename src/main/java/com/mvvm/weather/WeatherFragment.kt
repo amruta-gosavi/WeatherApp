933a0972111031de501452148fe51dd9de4082b4
Amruta-Gosavi <amrutagosavi29@gmail.com>
2023-01-08 15:30:00 -0500

    -initial commit

---
+++ src/main/java/com/mvvm/weather/WeatherFragment.kt
@@ -0,0 +1,36 @@
+package com.mvvm.weather
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mvvm.weather.databinding.WeatherFragmentBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class WeatherFragment : Fragment() {
+
+    private lateinit var _binding: WeatherFragmentBinding
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = WeatherFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+}
